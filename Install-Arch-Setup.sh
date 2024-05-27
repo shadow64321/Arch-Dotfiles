a60634f8ea2568eb918abb9ce869ba3eb361cbf5
MystrE <73987620+shadow64321@users.noreply.github.com>
2024-05-27 21:10:19 +1000

    Update Install-Arch-Setup.sh

--- Install-Arch-Setup.sh
+++ Install-Arch-Setup.sh
@@ -1,21 +1,26 @@
-sudo pacman -Syu &
+su root &
+
+pacman -Syu &
 
 # General Packages
-sudo pacman -S tar zsh steam vim neovim vlc network-manager-applet obs-studio syncthing rawtherapee gimp discord blanket lutris ffmpeg mpv aria2 tlp
+pacman -S tar zsh steam vim neovim vlc network-manager-applet obs-studio syncthing rawtherapee gimp discord blanket lutris ffmpeg mpv aria2 tlp &
 
 # BSPWM
-sudo pacman -S bspwm feh polybar picom sxhkd bc python-pywal dmenu scrot brightnessctl rofi sddm
+pacman -S bspwm feh polybar picom sxhkd bc python-pywal dmenu scrot brightnessctl rofi sddm &
 
 # Terminal Stuff
-sudo pacman -S mpd wget p7zip alacritty bat gdu lf btop neofetch git starship zellij asciiquarium cmatrix cowsay lolcat figlet zoxide fzf
+pacman -S mpd wget p7zip alacritty bat gdu lf btop neofetch git starship zellij asciiquarium cmatrix cowsay lolcat figlet zoxide fzf &
 
 # Programming Stuff
-sudo pacman -S gcc python cmake make
+pacman -S gcc python cmake make &
 
 # Infosec Stuff
-sudo pacman -S wireshark-cli okteta dhex nmap john
+pacman -S wireshark-cli okteta dhex nmap john &
 
 # Fonts
+pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-fira-code texlive-langkorean ttf-fantasque-nerd ttf-font-awesome
+
+# Failed Fonts: hachimarupop, terminus-nerdfont, siji, material-icons
 
 # Failed Packages: Anki, Floorp, Logseq, Mangal, musikcube, spotdl, libresprite, libreoffice, ascii-image-converter, cbonsai, bsp-layout, networkmanager_dmenu, betterlockscreen, gobuster, wordlists
 # Opt Packages: Clang, Rustup, libime, ghidra
