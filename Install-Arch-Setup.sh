fb756049c7e60ce15edf361158de0a938b394254
MystrE <73987620+shadow64321@users.noreply.github.com>
2024-05-27 20:26:40 +1000

    Create Install-Arch-Setup.sh

---
+++ Install-Arch-Setup.sh
@@ -0,0 +1,23 @@
+sudo pacman -Syu &
+
+# General Packages
+sudo pacman -S tar zsh steam vim neovim vlc network-manager-applet obs-studio syncthing rawtherapee gimp discord blanket lutris ffmpeg mpv aria2 tlp
+
+# BSPWM
+sudo pacman -S bspwm feh polybar picom sxhkd bc python-pywal dmenu scrot brightnessctl rofi sddm
+
+# Terminal Stuff
+sudo pacman -S mpd wget p7zip alacritty bat gdu lf btop neofetch git starship zellij asciiquarium cmatrix cowsay lolcat figlet zoxide fzf
+
+# Programming Stuff
+sudo pacman -S gcc python cmake make
+
+# Infosec Stuff
+sudo pacman -S wireshark-cli okteta dhex nmap john
+
+# Fonts
+
+# Failed Packages: Anki, Floorp, Logseq, Mangal, musikcube, spotdl, libresprite, libreoffice, ascii-image-converter, cbonsai, bsp-layout, networkmanager_dmenu, betterlockscreen, gobuster, wordlists
+# Opt Packages: Clang, Rustup, libime, ghidra
+
+# Emulation Packages: qemu, virt-manager
